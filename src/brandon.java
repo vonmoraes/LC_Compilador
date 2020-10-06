89acf2bb3330b6b88a0dda0c4c6f30070894fbe2
Lucas Cicutti <lucascicutti1995@gmail.com>
2020-10-06 20:23:54 -0300

    Create brandon.java

---
+++ src/brandon.java
@@ -0,0 +1,82 @@
+def betray(y,Sansa){
+	betray()
+}
+assert bar(19,( ( -0.77 ) )) : "A mind needs books as a sword needs a whetstone, if it is to keep its edge."
+assert ( bar(-protect(-Jon / 0.29 + ( -TABLE[-62][( x )] ) / Hodor * betray(ROWS / COLS - ( foo(-Arya,( -0.04 )) ),450.064)),destroy(( -( y ) * 1 ),( ( dog ) )) * x * 15 - rule() + -3 + 56) ) : "Nothing burns like the cold."
+def TABLE[0.88][j] {
+	TABLE[-COLS + 0.23][ROWS];
+	Arya /= -920.281 + -Sansa + rule(Jon,-340.942)
+}
+def mislead(Stark){
+	Arya -= rule(( 24 ));
+	Hodor -= Arya
+}
+def TABLE[betray(-430.305,( ( -970.9 ) - protect(Stark) + ( -0.22 ) / Arya - ROWS ))][i] {
+	if(x + destroy(( ( rule(-Hodor,x) ) ),( --0.45 ))){
+
+} else {
+	y *= ROWS
+};
+	if(protect()){
+	y *= COLS
+} else {
+	-ROWS;
+	Stark -= ROWS;
+	Ygritte -= ( betray(ROWS) ) + ---( mislead(-57,1,720.4) )
+}
+}
+def TABLE[dog][j] {
+	Ygritte /= ---( -dog ) * --73 / -27 + Jon
+}
+def protect(dog,Hodor,x){
+	Sansa *= 33;
+	Arya += 94 / --ROWS
+}
+def TABLE[Ygritte][j] {
+
+}
+def bar(Jon,Hodor){
+
+}
+var Jon = 29
+var Arya = ( -0.42 )
+def protect(){
+	if(y){
+	Ygritte *= 1;
+	if(protect(mislead(rule(( Sansa ))))){
+	Jon *= ( -COLS + Ygritte ) * --0.17;
+	if(( -350.1859 )){
+
+} else {
+	if(-foo()){
+
+}
+}
+}
+}
+}
+var Ygritte = ROWS
+var y = -Stark
+def TABLE[540.1][i] {
+	dog /= --ROWS + -( -TABLE[--600.7 + -TABLE[COLS][( --1 )]][TABLE[Hodor][Arya]] )
+}
+var x = TABLE[Jon][-TABLE[1][-280.961] * protect(97 + Jon)]
+var Hodor = mislead(25 - -0.07 + ( Stark ) * ( y ),COLS)
+var Hodor = Hodor
+var Hodor = 89
+def TABLE[-Sansa][k] {
+	if(-betray(COLS,protect(--760.601))){
+	Ygritte -= Hodor;
+	if(-81){
+	y *= Sansa;
+	-0.59
+};
+	( ROWS )
+} else {
+	Hodor -= TABLE[y][ROWS + ( Stark )];
+	y
+};
+	betray(-0.59);
+	bar(bar())
+}
+var Arya = ROWS
